3913bf8056b9bc36b51d233a963947cfbf62a522
ylgrst <125015797+ylgrst@users.noreply.github.com>
2023-05-12 12:01:37 +0200

    Add flexibility matrix definition

--- elana/stiffnessTensor.py
+++ elana/stiffnessTensor.py
@@ -9,11 +9,22 @@ class StiffnessTensor:
     """
 
     def __init__(self,
-                 matrix: npt.NDArray[np.float_] = np.zeros((6, 6))) -> None:
+                 matrix: npt.NDArray[np.float_] = np.zeros((6, 6)),
+                 flexibility_matrix: npt.NDArray[np.float_] = np.zeros((6, 6))) -> None:
         self.matrix = matrix
+        self.flexibility_matrix = flexibility_matrix
 
-    def read_matrix_from_txt(self, matrix_file : str) -> None:
+    def read_matrix_from_txt(self, matrix_file: str) -> None:
         matrix: npt.NDArray[np.float_] = np.loadtxt(matrix_file)
 
+        if matrix.shape != (6, 6):
+            raise ValueError("Matrix shape should be 6x6")
+        if np.linalg.norm(matrix - matrix.transpose()) > 1e-3:
+            raise ValueError("Matrix should be symmetric")
+
         self.matrix = matrix
 
+        try:
+            self.flexibility_matrix = np.linalg.inv(matrix)
+        except:
+            raise ValueError("Matrix is singular")
