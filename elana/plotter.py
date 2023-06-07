0c080c7334461eee560c8258056a248855c42788
ylgrst <125015797+ylgrst@users.noreply.github.com>
2023-06-07 11:40:13 +0200

    Implement 3D plotter for linear compressibility

--- elana/plotter.py
+++ elana/plotter.py
@@ -5,6 +5,27 @@ import matplotlib.pyplot as plt
 from matplotlib import cm, colors
 
 
+def _symmetrical_colormap(cmap_settings, new_name=None):
+    ''' This function take a colormap and create a new one, as the concatenation of itself by a symmetrical fold.
+    '''
+    # get the colormap
+    cmap = plt.cm.get_cmap(*cmap_settings)
+    if not new_name:
+        new_name = "sym_" + cmap_settings[0]  # ex: 'sym_Blues'
+
+    # this defines the roughness of the colormap, 128 fine
+    n = 128
+
+    # get the list of color from colormap
+    colors_r = cmap(np.linspace(0, 1, n))  # take the standard colormap # 'right-part'
+    colors_l = colors_r[::-1]  # take the first list of color and flip the order # "left-part"
+
+    # combine them and build a new colormap
+    new_colors = np.vstack((colors_l, colors_r))
+    my_map = colors.LinearSegmentedColormap.from_list(new_name, new_colors)
+
+    return my_map
+
 def plot_young_3d(stiffness_matrix: StiffnessTensor) -> None:
     """3D plotter for Young modulus"""
 
@@ -18,27 +39,27 @@ def plot_young_3d(stiffness_matrix: StiffnessTensor) -> None:
     data_z = np.zeros((n_points, 2*n_points))
     data_young = np.zeros((n_points, 2*n_points))
 
-    for i in range(n_points):
-        for j in range(2*n_points):
-            young = stiffness_matrix.young((theta_array[i], phi_array[j]))
-            data_young[i,j] = young
-            z = young * np.cos(theta_array[i])
-            x = young * np.sin(theta_array[i]) * np.cos(phi_array[j])
-            y = young * np.sin(theta_array[i]) * np.sin(phi_array[j])
-            data_x[i,j] = x
-            data_y[i,j] = y
-            data_z[i,j] = z
+    for index_theta in range(len(theta_array)):
+        for index_phi in range(len(phi_array)):
+            young = stiffness_matrix.young((theta_array[index_theta], phi_array[index_phi]))
+            data_young[index_theta,index_phi] = young
+            z = young * np.cos(theta_array[index_theta])
+            x = young * np.sin(theta_array[index_theta]) * np.cos(phi_array[index_phi])
+            y = young * np.sin(theta_array[index_theta]) * np.sin(phi_array[index_phi])
+            data_x[index_theta,index_phi] = x
+            data_y[index_theta,index_phi] = y
+            data_z[index_theta,index_phi] = z
 
     young_average = np.average(data_young)
     young_min = np.min(data_young)
     young_max = np.max(data_young)
 
-    figure = plt.figure()
+    plt.figure()
     axes = plt.axes(projection='3d')
 
     norm = colors.Normalize(vmin=young_min, vmax=young_max, clip=False)
 
-    surface = axes.plot_surface(data_x, data_y, data_z, norm=norm, cmap='viridis')
+    axes.plot_surface(data_x, data_y, data_z, norm=norm, cmap='viridis')
 
     scalarmap = cm.ScalarMappable(cmap='viridis', norm=norm)
     scalarmap.set_clim(young_min, young_max)
@@ -71,18 +92,24 @@ def plot_linear_compressibility_3d(stiffness_matrix: StiffnessTensor) -> None:
     data_y_neg = np.zeros((len(theta_array), len(phi_array)))
     data_z_neg = np.zeros((len(theta_array), len(phi_array)))
 
+    data_linear_compressibility_pos = np.zeros((n_points, 2*n_points))
+    data_linear_compressibility_neg = np.zeros((n_points, 2 * n_points))
+
     for index_theta in range(len(theta_array)):
         for index_phi in range(len(phi_array)):
-            tmp_pos = max(0.0, stiffness_matrix.linear_compressibility((theta_array[index_theta], phi_array[index_phi])))
-            tmp_neg = max(0.0, stiffness_matrix.linear_compressibility((theta_array[index_theta], phi_array[index_phi])))
+            linear_compressibility_pos = max(0.0, stiffness_matrix.linear_compressibility((theta_array[index_theta], phi_array[index_phi])))
+            linear_compressibility_neg = max(0.0, -stiffness_matrix.linear_compressibility((theta_array[index_theta], phi_array[index_phi])))
 
-            x_pos = tmp_pos * np.sin(theta_array[index_theta]) * np.cos(phi_array[index_phi])
-            y_pos = tmp_pos * np.sin(theta_array[index_theta]) * np.sin(phi_array[index_phi])
-            z_pos = tmp_pos * np.cos(theta_array[index_theta])
+            data_linear_compressibility_pos[index_theta, index_phi] = linear_compressibility_pos
+            data_linear_compressibility_neg[index_theta, index_phi] = linear_compressibility_neg
 
-            x_neg = tmp_neg * np.sin(theta_array[index_theta]) * np.cos(phi_array[index_phi])
-            y_neg = tmp_neg * np.sin(theta_array[index_theta]) * np.sin(phi_array[index_phi])
-            z_neg = tmp_neg * np.cos(theta_array[index_theta])
+            x_pos = linear_compressibility_pos * np.sin(theta_array[index_theta]) * np.cos(phi_array[index_phi])
+            y_pos = linear_compressibility_pos * np.sin(theta_array[index_theta]) * np.sin(phi_array[index_phi])
+            z_pos = linear_compressibility_pos * np.cos(theta_array[index_theta])
+
+            x_neg = linear_compressibility_neg * np.sin(theta_array[index_theta]) * np.cos(phi_array[index_phi])
+            y_neg = linear_compressibility_neg * np.sin(theta_array[index_theta]) * np.sin(phi_array[index_phi])
+            z_neg = linear_compressibility_neg * np.cos(theta_array[index_theta])
 
             data_x_pos[index_theta][index_phi] = x_pos
             data_y_pos[index_theta][index_phi] = y_pos
@@ -91,3 +118,30 @@ def plot_linear_compressibility_3d(stiffness_matrix: StiffnessTensor) -> None:
             data_x_neg[index_theta][index_phi] = x_neg
             data_y_neg[index_theta][index_phi] = y_neg
             data_z_neg[index_theta][index_phi] = z_neg
+
+    linear_compressibility_max = max(data_linear_compressibility_pos)
+    linear_compressibility_min = min(data_linear_compressibility_neg)
+
+    plt.figure()
+    axes = plt.axes(projection='3d')
+
+    norm_pos = colors.Normalize(vmin=0.0, vmax=linear_compressibility_max, clip=False)
+    norm_neg = colors.Normalize(vmin=linear_compressibility_min, vmax=0.0, clip=False)
+    norm = colors.Normalize(vmin=linear_compressibility_min, vmax=linear_compressibility_max, clip=False)
+
+    axes.plot_surface(data_x_pos, data_y_pos, data_z_pos, norm=norm_pos, cmap='viridis')
+    axes.plot_surface(data_x_neg, data_y_neg, data_z_neg, norm=norm_neg, cmap='viridis')
+
+    scalarmap = cm.ScalarMappable(cmap='viridis', norm=norm)
+    scalarmap.set_clim(linear_compressibility_min, linear_compressibility_max)
+
+    cbar = plt.colorbar(scalarmap, orientation="horizontal", fraction=0.06, pad=-0.1, ticks=[linear_compressibility_min, 0.0, linear_compressibility_max])
+    cbar.ax.tick_params(labelsize='large')
+    cbar.set_label(r'directional stiffness $E$ (MPa)', size=15, labelpad=20)
+
+    axes.figure.axes[1].tick_params(axis="x", labelsize=20)
+    axes.azim = 30
+    axes.elev = 30
+
+    plt.savefig("directional_linear_compressibility.png", transparent=True)
+    plt.show()
